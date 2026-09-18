45b4dc73cbfb6b42ca3e832ed123a29bb8f7f5a5
Gabriel Selzer <gjselzer@wisc.edu>
2026-09-18 11:28:23 -0500

    Use dependency group syntax

--- noxfile.py
+++ noxfile.py
@@ -9,5 +9,6 @@ nox.options.sessions = ["test"]
 
 @nox.session(python=["3.10", "3.11", "3.12", "3.13"])
 def test(session):
-    session.install(".[testing]")
+    session.install(".")
+    session.install("--group", "testing")
     session.run("pytest")
